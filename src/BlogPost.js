9b05f539676126f987de4b90ca896aebc5314ca5
Diego Amicabile <diego.amicabile@gmail.com>
2018-03-06 22:28:54 +0100

    Blogpost

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -3,14 +3,47 @@ import App from "./App";
 
 class BlogPost extends React.Component {
 
+    constructor() {
+        super();
+
+
+        this.state = {
+            content: "",
+            isToggleOn: false
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick( ) {
+        this.setState(prevState => ({
+            isToggleOn: !prevState.isToggleOn
+        }));
+    }
+
+
+    componentDidMount() {
+        this.setState({
+            content : this.props.content.replace(/\n\s*\n/g, '\n'),
+            isToggleOn: false
+        });
+    }
 
     render() {
+        let content = ""
+        if (this.state.isToggleOn) {
+            content = this.state.content
+        }
+
         return (
-            <div>
+            <div >
                 <p>
-                    <span key={ this.props.index }>{this.props.title}</span>
+                    <a href="#" onClick={this.handleClick}>
+                        <span key={ this.props.index }>{ this.props.title }</span>
+                    </a>
                 </p>
-                <p class="content">{ this.props.content }</p>
+                <p class="content">
+                    { content }
+                </p>
             </div>
         )
     }
